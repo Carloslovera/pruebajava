367c93bdc62d63091a51f0d9a2e58a5305716184
Frost <carloslovera1314@gmaill.com>
2026-02-03 16:34:13 -0400

    primera subida

---
+++ appjava/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 18;
+        byte edad = 15;
+        short cuenta = -2;
+        long numerosEnteros = 5;
+
+        System.out.println(age);
+        System.out.println(edad);
+        System.out.println(cuenta);
+        System.out.println(numerosEnteros);
+
+        float decimales = 15.3f;
+        double ultimosDecimales = 18.5;
+
+        System.out.println(decimales);
+        System.out.println(ultimosDecimales);
+
+        boolean sonGeniales = true;
+        boolean dicenVerdadoMentira = false;
+
+        System.out.println(sonGeniales);
+        System.out.println(dicenVerdadoMentira);
+
+        char caracteres = 'a';
+        System.out.println(caracteres);
+
+        String hola = ("hola mundo");
+        System.out.println(hola);
+
+    }
+}
